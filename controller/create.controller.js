50383e04b54d22382dcc7d3f5c2276424ac96d4a
minhtam12356 <tam26918@gmail.com>
2021-02-04 01:30:18 +0700

    ok

--- controller/create.controller.js
+++ controller/create.controller.js
@@ -1,5 +1,12 @@
 var userModel = require('../models/user.model');
+
 var md5 = require('md5');
+var cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
 module.exports.create = function(req, res){
     res.render('create')
@@ -9,7 +16,8 @@ module.exports.postCreate = async function(req,res){
     delete req.body.cpass;
 
     if (req.file){
-        req.body.avatar = (req.file.destination + req.file.filename).split('/').slice(2, 4).join('/');
+        var upload = await cloudinary.uploader.upload(req.file.path);
+        req.body.avatar = upload.url;
     }
     else{
         req.body.avatar = "avatar.png";

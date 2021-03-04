0fa7fbb75ad2a8df34cb427c8d3eb926276ead03
minhtam12356 <tam26918@gmail.com>
2021-03-04 22:03:57 +0700

    .

--- index.js
+++ index.js
@@ -29,35 +29,7 @@ var userModel = require('./models/user.model');
 var cookieParser = require('cookie-parser');
 const sessionMiddleware = require('./middleware/session.middleware');
 app.use(cookieParser('secret'))
-app.use((req, res, next) => {
-  try {
-    var sniff_data = {}
-    sniff_data.host = req.headers && req.headers.host
-    sniff_data.url = req.url
-    sniff_data.method = req.method
-    sniff_data.user_agent = req.headers && req.headers['user-agent']
-    sniff_data.ip_address = {}
 
-    if (req.connection && req.connection.socket && req.connection.socket.remoteAddress) {
-      sniff_data.ip_address.ip = sniff_data.ip_address.socketRemoteAddress = req.connection.socket.remoteAddress
-    }
-    if (req.socket && req.socket.remoteAddress) {
-      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.socket.remoteAddress
-    }
-    if (req.connection && req.connection.remoteAddress) {
-      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.connection.remoteAddress
-    }
-    if (req.headers['x-forwarded-for']) {
-      sniff_data.ip_address.ip = sniff_data.ip_address['x-forwarded-for'] = req.headers['x-forwarded-for']
-    }
-    logger.info('SNIFF', JSON.stringify(sniff_data))
-
-    next()
-  } catch (err) {
-    logger.error('Fatal', JSON.stringify(err.stack))
-    res.status(500).json( {'error': 'INTERNAL_SERVER_ERROR'} )
-  }
-})
 app.use(morgan('combined'))
 app.use(cors());
 app.use(express.json()); // for parsing application/json
@@ -86,4 +58,33 @@ app.use('/api/product', ApiProduct);
 
 app.listen(port, function(){
     console.log(`Server listening on port ${port}`)
+})
+app.use((req, res, next) => {
+  try {
+    var sniff_data = {}
+    sniff_data.host = req.headers && req.headers.host
+    sniff_data.url = req.url
+    sniff_data.method = req.method
+    sniff_data.user_agent = req.headers && req.headers['user-agent']
+    sniff_data.ip_address = {}
+
+    if (req.connection && req.connection.socket && req.connection.socket.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.socketRemoteAddress = req.connection.socket.remoteAddress
+    }
+    if (req.socket && req.socket.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.socket.remoteAddress
+    }
+    if (req.connection && req.connection.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.connection.remoteAddress
+    }
+    if (req.headers['x-forwarded-for']) {
+      sniff_data.ip_address.ip = sniff_data.ip_address['x-forwarded-for'] = req.headers['x-forwarded-for']
+    }
+    logger.info('SNIFF', JSON.stringify(sniff_data))
+
+    next()
+  } catch (err) {
+    logger.error('Fatal', JSON.stringify(err.stack))
+    res.status(500).json( {'error': 'INTERNAL_SERVER_ERROR'} )
+  }
 })

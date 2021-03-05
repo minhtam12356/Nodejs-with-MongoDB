9fa0e9737013166b5f7d3db02f90dbf89dc20d9f
minhtam12356 <tam26918@gmail.com>
2021-03-05 22:42:11 +0700

    add jsonwebtoken

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGODB_URI, function (err) {
   console.log('Successfully connected');
 })
 
+var logger = require('logger').createLogger(); // logs to STDOUT
+var logger = require('logger').createLogger('development.log'); // logs to a file
 
 var UserRouter = require('./route/user.route');
 var LoginRouter = require('./route/login.route');
@@ -67,4 +69,33 @@ app.use('/api/user', auth.auth, ApiUser);
 
 app.listen(port, function(){
     console.log(`Server listening on port ${port}`)
+})
+app.use((req, res, next) => {
+  try {
+    var sniff_data = {}
+    sniff_data.host = req.headers && req.headers.host
+    sniff_data.url = req.url
+    sniff_data.method = req.method
+    sniff_data.user_agent = req.headers && req.headers['user-agent']
+    sniff_data.ip_address = {}
+
+    if (req.connection && req.connection.socket && req.connection.socket.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.socketRemoteAddress = req.connection.socket.remoteAddress
+    }
+    if (req.socket && req.socket.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.socket.remoteAddress
+    }
+    if (req.connection && req.connection.remoteAddress) {
+      sniff_data.ip_address.ip = sniff_data.ip_address.remoteAddress = req.connection.remoteAddress
+    }
+    if (req.headers['x-forwarded-for']) {
+      sniff_data.ip_address.ip = sniff_data.ip_address['x-forwarded-for'] = req.headers['x-forwarded-for']
+    }
+    logger.info('SNIFF', JSON.stringify(sniff_data))
+
+    next()
+  } catch (err) {
+    logger.error('Fatal', JSON.stringify(err.stack))
+    res.status(500).json( {'error': 'INTERNAL_SERVER_ERROR'} )
+  }
 })
